a8a82ab239b51b114199483684aa46ad3f13808c
tuanagenc028@gmail.com <tuanagenc028@gmail.com>
2024-03-21 20:36:46 +0300

    Mini calendar application finished

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Frida
 
 const allMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-date.innerHTML = today.getDate();
+date.innerHTML = (today.getDate() < 10 ? "0" : "") + today.getDate();
 day.innerHTML = weekDays[today.getDay()];
 month.innerHTML = allMonths[today.getMonth()];
 year.innerHTML = today.getFullYear();
